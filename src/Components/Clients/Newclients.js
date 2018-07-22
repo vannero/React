7fc40439982bda4f8b5e7e800f78f7a3723125c6
Ivan Christopher Cruz <cruzi@rchsp.med.sa>
2018-07-22 14:23:37 +0300

    22-7-2017

--- src/Components/Clients/Newclients.js
+++ src/Components/Clients/Newclients.js
@@ -5,13 +5,14 @@ import fire from '../../fire';
 import '../../App.css';
 
 class Newclients extends Component {
-    constructor(props, context) {
-        super(props, context);
+    constructor(props) {
+        super(props);
 
         this.handleChange = this.handleChange.bind(this);
 
         this.state = {
             value: '',
+            ready: false,
             nationality:[{
                 id:'',
                 nationality:''
@@ -19,6 +20,10 @@ class Newclients extends Component {
             area:[{
                 id:'',
                 location:''
+            }],
+            property:[{
+                id:'',
+                property:''
             }]
         };
     }
@@ -57,18 +62,29 @@ class Newclients extends Component {
     }
 
     getProperty(){
-        
+        let self=this;
+        fire.database().ref('Property').on('value', snapshot => {
+            snapshot.forEach(function(childSnapshot) {
+                let property={
+                    id:childSnapshot.key,
+                    property :childSnapshot.val().Property,
+                }
+                //this.setState({ myArray: [...this.state.myArray, ...[1,2,3] ] }) //another array
+                self.setState({property:[... self.state.property, ...[property]]});
+            });    
+            
+        })
     }
 
-
-
-
-    
-
-    componentDidMount() {
+    componentWillMount(){
         this.getNationality();
         this.getArea();
         this.getProperty();
+    }
+
+
+    componentDidMount() {
+        this.setState({ ready: true })
     }
 
     getValidationState() {
@@ -82,132 +98,125 @@ class Newclients extends Component {
     handleChange(e) {
         this.setState({ value: e.target.value });
     }
-    render() {
-        
+
+    loading() {
+        return (
+            <div>loading</div>
+        )
+    }
+
+    preload() {
         let optionItems = this.state.nationality.map((nationality) =>
-                <option key={nationality.id} value={nationality.id}>{nationality.nationality}</option>
-            ); 
+            <option key={nationality.id} value={nationality.id}>{nationality.nationality}</option>
+        );
 
         let Area = this.state.area.map((area) =>
             <option key={area.id} value={area.id}>{area.location}</option>
-        );     
-     
-        return (
-
-            <div className="container">
-                <Panel>
-                    <Panel.Heading>New Clients</Panel.Heading>
-
-                    <Panel.Body>
-                        <Form>
-                            <h5 className="card-title">Personal Information</h5>
-                            <hr />
-                            <Col xs={6} md={4}>
-                                <FormGroup>
-                                    <Label htmlFor="name">Name (English)</Label>
-                                     <Input type="text" id="NameEn" placeholder="Enter your name" required />
-                                </FormGroup>
-                            </Col>
-                            <Col xs={6} md={4}>
-                                <FormGroup>
-                                <Label htmlFor="name">Name (Korean)</Label>
-                                     <Input type="text" id="NameKn" placeholder="Enter your name" required />
-                                </FormGroup>
-                            </Col>
-                            <Col  md={4}>
-                                <FormGroup>
-                                    <Label htmlFor="Nationality">Nationality</Label>
-                                    <Input type="select" name="Nationality" id="Nationality">
-                                   {optionItems}
-                                    </Input>
-                                    
-                                    
-                                </FormGroup>
-                            </Col>
-
-                            <Col xs={6} md={4}>
-                                <FormGroup>
-                                <Label htmlFor="Mobile">Mobile</Label>
-                                     <Input type="text" id="Mobile" placeholder="Enter your mobile" required />
-                                </FormGroup>
-                            </Col>
-                            <Col xs={6} md={4}>
-                                <FormGroup>
-                                <Label htmlFor="Email">Email</Label>
-                                     <Input type="email" id="Email" placeholder="Enter your Email" required />
-                                </FormGroup>
-                            </Col>
-                            <Col md={4}>
-                                <FormGroup>
-                                <Label htmlFor="Kakaoid">Kakao Id</Label>
-                                     <Input type="text" id="Kakaoid" placeholder="Enter your Kakaoid" required />
-                                </FormGroup>
-                            </Col>
-
-                            <h5 className="card-title">Address</h5>
-                            <hr />
-
-                            <Col xs={6} md={4}>
-                                <FormGroup>
-                                <Label htmlFor="HouseNo">House No.</Label>
-                                     <Input type="text" id="HouseNo" placeholder="Enter your House Number" required />
-                                </FormGroup>
-                            </Col>
-                            <Col xs={6} md={4}>
-                                <FormGroup>
-                                <Label htmlFor="BuidlingNo">Building / House Address</Label>
-                                     <Input type="text" id="BuidlingNo" placeholder="Enter your House Buidling or House Number" required />
-                                </FormGroup>
-                            </Col>
-                            <Col md={4}>
-                                <FormGroup>
-                                <Label htmlFor="Area">Area</Label>
-                                <Input type="select" name="Nationality" id="Nationality">
-                                   {Area}
-                                    </Input>
-                                </FormGroup>
-                            </Col>
-
-                            <Col xs={6} md={4}>
-                                <FormGroup>
-                                <Label htmlFor="Areacode">Area Code</Label>
-                                     <Input type="text" id="Areacode" placeholder="Enter your Areacode" required />
-                                </FormGroup>
-                            </Col>
-                            <Col xs={6} md={4}>
-                                <FormGroup>
-                                    <Label htmlFor="Propertytype">Property Type</Label>
-                                    <Input type="select" name="Propertytype" id="Propertytype">
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
-                                        <option value="6">6</option>
-                                        <option value="7">7</option>
-                                        <option value="8">8</option>
-                                        <option value="9">9</option>
-                                        <option value="10">10</option>
-                                        <option value="11">11</option>
-                                        <option value="12">12</option>
-                                    </Input>
-                                </FormGroup>
-                                
-                            </Col>
-
-
-
-
-
-
-
-
-
-                        </Form>
-                    </Panel.Body>
-                </Panel>
-            </div>
         );
+
+        let Property = this.state.property.map((property) =>
+            <option key={property.id} value={property.id}>{property.property}</option>
+        );
+        return (
+            <div className="container">
+            <Panel>
+                <Panel.Heading>New Clients</Panel.Heading>
+
+                <Panel.Body>
+                    <Form>
+                        <h5 className="card-title">Personal Information</h5>
+                        <hr />
+                        <Col xs={6} md={4}>
+                            <FormGroup>
+                                <Label htmlFor="name">Name (English)</Label>
+                                 <Input type="text" id="NameEn" placeholder="Enter your name" required />
+                            </FormGroup>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <FormGroup>
+                            <Label htmlFor="name">Name (Korean)</Label>
+                                 <Input type="text" id="NameKn" placeholder="Enter your name" required />
+                            </FormGroup>
+                        </Col>
+                        <Col  md={4}>
+                            <FormGroup>
+                                <Label htmlFor="Nationality">Nationality</Label>
+                                <Input type="select" name="Nationality" id="Nationality">
+                               {optionItems}
+                                </Input>
+                            </FormGroup>
+                        </Col>
+
+                        <Col xs={6} md={4}>
+                            <FormGroup>
+                            <Label htmlFor="Mobile">Mobile</Label>
+                                 <Input type="text" id="Mobile" placeholder="Enter your mobile" required />
+                            </FormGroup>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <FormGroup>
+                            <Label htmlFor="Email">Email</Label>
+                                 <Input type="email" id="Email" placeholder="Enter your Email" required />
+                            </FormGroup>
+                        </Col>
+                        <Col md={4}>
+                            <FormGroup>
+                            <Label htmlFor="Kakaoid">Kakao Id</Label>
+                                 <Input type="text" id="Kakaoid" placeholder="Enter your Kakaoid" required />
+                            </FormGroup>
+                        </Col>
+
+                        <h5 className="card-title">Address</h5>
+                        <hr />
+
+                        <Col xs={6} md={4}>
+                            <FormGroup>
+                            <Label htmlFor="HouseNo">House No.</Label>
+                                 <Input type="text" id="HouseNo" placeholder="Enter your House Number" required />
+                            </FormGroup>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <FormGroup>
+                            <Label htmlFor="BuidlingNo">Building / House Address</Label>
+                                 <Input type="text" id="BuidlingNo" placeholder="Enter your House Buidling or House Number" required />
+                            </FormGroup>
+                        </Col>
+                        <Col md={4}>
+                            <FormGroup>
+                            <Label htmlFor="Area">Area</Label>
+                            <Input type="select" name="Area" id="Area">
+                               {Area}
+                                </Input>
+                            </FormGroup>
+                        </Col>
+
+                        <Col xs={6} md={4}>
+                            <FormGroup>
+                            <Label htmlFor="Areacode">Area Code</Label>
+                                 <Input type="text" id="Areacode" placeholder="Enter your Areacode" required />
+                            </FormGroup>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <FormGroup>
+                                <Label htmlFor="Propertytype">Property Type</Label>
+                                <Input type="select"name="Propertytype" id="Propertytype">
+                                    {Property}
+                                </Input>
+                            </FormGroup>
+                            
+                        </Col>
+                    </Form>
+                </Panel.Body>
+            </Panel>
+        </div>
+        );
+    }
+    render() {
+        if (this.state.ready) {
+            return this.preload()
+        } else {
+            return this.loading();
+        }
     }
 }
 
